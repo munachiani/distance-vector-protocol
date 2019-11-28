e744d3f76e4dd8445f0f99ffa196e3c9172577c0
Saul <saulc@users.noreply.github.com>
2019-11-28 02:03:32 -0800

    moved to dev branch
    made classs. not sure what it needs to do yet. only the main server reads it?

--- ReadConfig.py
+++ ReadConfig.py
@@ -1,11 +1,12 @@
 
 # config file format
 # <num-servers>
-# • <num-neighbors>
-# • <server-ID> <server-IP> <server-port>
-# • <server-ID1> <server-ID2> <cost>
+# <num-neighbors>
+# <server-ID> <server-IP> <server-port>
+# <server-ID1> <server-ID2> <cost>
 # todo class type for server, id, ip, port, costtoserver1
 
+import TopoServer
 
 def openFile():
     file = open("config.txt", 'r')
@@ -14,9 +15,15 @@ def openFile():
 def printFile(f):
     print(f.read())
 
+def getServers(f):
+    s = f.readline(1)
+    return str(s)
+    # for i in range(0, int(s))
+
 
 
 
 if __name__ == '__main__':
     config = openFile()
+    print("Servers in this config: " + getServers(config))
     printFile(config)
---
+++ TopoServer.py
@@ -0,0 +1,18 @@
+
+
+class TopoServer:
+    def __init__(self, id, ip, port, c):
+        self.id = id
+        self.ip = ip
+        self.port = port
+        self.cost = c
+
+
+        def getIp(self): return self.ip
+        def getId(self): return self.id
+        def getPort(self): return self.port
+        def getCost(self): return self.cost
+
+
+
+        
