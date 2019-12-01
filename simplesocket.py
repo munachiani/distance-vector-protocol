d6d125f6cdd08a2b84e97a8be6350658f30735bb
Saul <saulc@users.noreply.github.com>
2019-12-01 01:44:02 -0800

    socket
    updating connection for tests

--- simplesocket.py
+++ simplesocket.py
@@ -1,18 +1,19 @@
 # https://www.geeksforgeeks.org/socket-programming-python/
-# used as reference for socket server.
+# https://realpython.com/python-sockets/#multi-connection-client-and-server
+#used as reference for socket server.
 import socket
 
 
 class Simplesocket:
     def __init__(self, id, ip, port):
-        self.s = socket.socket()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.port = port
         self.id = id
         self.ip = ip
         self.s.bind(('', port))
-        self.s.listen(5)
 
     def listen(self):
+        self.s.listen()
         self.c, addr = self.s.accept()
         print(" Connected to: " + addr)
         self.c.send('Thank you for connecting')
